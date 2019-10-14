6ac99399924a61ac5ae4b7b7655d5b2d271e1aa2
iAlzar <srichardson97402@hotmail.com>
2019-10-13 19:00:44 -0700

    Convert to new component system

--- server/main.lua
+++ server/main.lua
@@ -1,12 +1,12 @@
 RegisterServerEvent('mythic_hud:server:GetMoneyStuff')
 AddEventHandler('mythic_hud:server:GetMoneyStuff', function()
     local cData = {}
-    while exports['mythic_base']:getPlayerFromId(source) == nil do
+    while exports['mythic_base']:FetchComponent('Fetch'):Source(source) == nil do
         Citizen.Wait(0)
     end
-    while exports['mythic_base']:getPlayerFromId(source).getChar() == nil do
+    while exports['mythic_base']:FetchComponent('Fetch'):Source(source):GetData('character') == nil do
         Citizen.Wait(0)
     end
-    cData = exports['mythic_base']:getPlayerFromId(source).getChar().getCharData()
+    cData = exports['mythic_base']:FetchComponent('Fetch'):Source(source):GetData('character'):GetData()
     TriggerClientEvent('mythic_hud:client:DisplayMoneyStuff', source, cData.cash, cData.bank)
 end)
